541b768526462b8f0aff2e21673eaa584fa4cd58
Mohammad saeed <sk.saeed008@gmial.com>
2024-04-17 10:59:02 +0530

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by saeed shaikh on 4/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
